7872658e4ee49edfa1f179b0ddf8892ad23b37b3
40823125 <40823125@gm.nfu.edu.tw>
2019-11-14 21:44:24 +0800

    add quizlet

---
+++ downloads/c,f/c,f.dart
@@ -0,0 +1,13 @@
+main(){
+var c;
+var f;
+double k1;
+double k2;
+c=32;    
+k1=(9/5)*c+32;
+print('The value of x at f=$f is: ${k1}');
+f=32;
+k2=(f-32)*9/5;  
+print('The value of x at c=$c is: ${k2}');
+}
+   
