ae4e43eef482813481de569c4c4aa0de8fddfeb8
40823125 <55352191+40823125@users.noreply.github.com>
2019-12-04 21:50:31 +0800

    Update string.dart

--- downloads/dart_ex/list string/string.dart
+++ downloads/dart_ex/list string/string.dart
@@ -1,31 +1,29 @@
 main(){  
-//字串temp40C,80C,50F,40C,25F
+//字串temp為[40C,80C,50F,40C,25F]
  List temp = ["40C", "80C", "50F", "40C", "25F"];
   for (var i in temp) {
+  var a = int.parse(i[0]+i[1]);
+  //將字串轉數字
     if (i[2] == "C") {
     print(i); 
-    var a = int.parse(i[0]+i[1]);
     double k=(9/5)*a+32;
-    print(k);
+    print("$k F");
 }
     else{
     print(i); 
-    var y = int.parse(i[0]+i[1]);
-    double m=(y-32)*5/9;
-    print(m);
+    double m=(a-32)*5/9;
+    print("$m C");
 /*
  for迴圈使i=temp
  if當i字串第三個字是C時,執行
     print(i);
-    var a = int.parse(i[0]+i[1]);
     double k=(9/5)*a+32;
     print(k);
   else否時,執行
     print(i); 
-    var y = int.parse(i[0]+i[1]);
     double m=(y-32)*5/9;
     print(m);
  */
 }
 }
-}   
+}  
