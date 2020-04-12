3fbb5dd4635db5ede46b119c0da6509ccb56fc34
40823125 <40823125@gm.nfu.edu.tw>
2020-04-12 16:04:40 +0800

    ST更新

--- downloads/dart_ex/ST/ST.dart
+++ downloads/dart_ex/ST/ST.dart
@@ -3,6 +3,8 @@ import 'dart:html';
  
   InputElement studListUrl = querySelector("#studListUrl");
   String studUrl;
+  InputElement st = querySelector("#Url");
+  String stU;
   // 將 Label 改為 Textarea, 避免產生過程結果嵌入所在頁面
   TextAreaElement output = querySelector("#output");
  
@@ -16,9 +18,15 @@ grouping() {
   if (studListUrl.value != "") {
     studUrl = studListUrl.value;
   } else {
-    studUrl = 'https://mde.tw/group/downloads/2019fall_cp_1a_list.txt';
+    studUrl = 'https://40823125.github.io/cp2019/downloads/9zk6c-82h5m.txt';
   }
- 
+
+  if (st.value != "") {
+    stU = st.value;
+  } else {
+    studUrl = 'https://40823125.github.io/cp2019/downloads/al5ts-tl0s0.txt';
+  }
+  
   // 組序由 1 開始
   int gth = 1;
   // 迴圈序號變數
@@ -32,7 +40,13 @@ grouping() {
   var group = [];
   // 各組人數數列
   var numList = [];
- 
+  // 每組學員暫存數列
+  var gp = [];
+  // 全班分組數列
+  var gro = [];
+  // 各組人數數列
+  var numL = [];
+  
   HttpRequest.getString(studUrl).then((String resp) {
     // 利用 trim() 去除字串最後的跳行符號, 之後再利用 split() 根據 \n 轉為數列
     var studList = resp.trim().split("\n");
@@ -53,9 +67,56 @@ grouping() {
       group.add(gpList);
     }
   });
+   output.text += '=' * 20 + "\n";
+     HttpRequest.getString(stU).then((String sp) {
+    // 利用 trim() 去除字串最後的跳行符號, 之後再利用 split() 根據 \n 轉為數列
+    var st = sp.trim().split("\n");
+    numList = getN(st.length);
+    inc = 0;
+    for (i in numL){
+      gp = [];
+      for (j = 0; j < i; j++){
+        output.text += st[j+inc] + "\n";
+        // 在各分組數列中加入將對應的學員學號
+        gp.add(st[j+inc]);
+      }
+      gth = gth + 1;
+      inc = inc + j;
+        //output.text += studList[j] + "\n";
+        // 逐步將各組暫存的分組數列加入全班分組數列中
+      gp.sort();
+      gro.add(gp);
+    }
+  });
 }
  
 List getNumList(int total){
+  // total student number
+  // int total = 65;
+  // initial each group expect to be "eachGrp" number of people
+  int ea = 5;
+  // may divide into "grpNum" number of group
+  int grp = total ~/ ea;
+  // vacant list
+  var sp = [];
+  // find remainder when total number divid into "grpNum" number of group
+  int remainder = total % grp;
+  // number of people in one group by calculation
+  int calGrp = total ~/ grp;
+ 
+  for (int i = 0; i < grp; i++) {
+    sp.add(calGrp);
+  }
+  //print(splits);
+ 
+  for (int i = 0; i < remainder; i++) {
+    sp[i] += 1;
+  }
+  //print(splits);
+  return sp;
+ }
+
+List getN(int total){
   // total student number
   // int total = 65;
   // initial each group expect to be "eachGrp" number of people
